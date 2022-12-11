14822be067d9e32108251ee08f49ccc1903ae1d9
vaibhavwabale <vaibhavsonu0506@gmail.com>
2022-12-11 19:36:09 +0530

    Added Splash Screen & Filter Screen

---
+++ lib/Screens/filter.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+
+class Filter extends StatefulWidget {
+  const Filter({Key? key}) : super(key: key);
+
+  @override
+  State<Filter> createState() => _FilterState();
+}
+
+enum Levels { Alllevels, Beginner, Intermediate, Confirmed }
+
+class _FilterState extends State<Filter> {
+  String dropdownValue = 'Marks';
+  List<String> filters = [
+    'Marks',
+    'Most Recents',
+    'By Prices, From Lowest To Highest',
+    'By Prices From Highest To Lowest'
+  ];
+  Levels? character = Levels.Alllevels;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+              child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(20),
+                  child: SizedBox(
+                    height: size.height,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InkWell(
+                          onTap: () async {
+                            await Future.delayed(
+                                const Duration(milliseconds: 300));
+                            Navigator.pop(context);
+                          },
+                          child: Ink(
+                              child: Container(
+                                  height: 35,
+                                  width: 35,
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.grey.shade300),
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: const Icon(
+                                      Icons.arrow_back_ios_outlined,
+                                      color: Colors.grey,
+                                      size: 15))),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text(
+                          '10 000 Courses',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          'Filter By',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        DropdownButton<String>(
+                          value: dropdownValue,
+                          icon: const Icon(Icons.arrow_downward),
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.deepPurple),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.deepPurpleAccent,
+                          ),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownValue = newValue!;
+                            });
+                          },
+                          items: filters
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          'Level',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ListTile(
+                                title: const Text('All Levels'),
+                                leading: Radio<Levels>(
+                                  value: Levels.Alllevels,
+                                  groupValue: character,
+                                  onChanged: (Levels? value) {
+                                    setState(() {
+                                      character = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              ListTile(
+                                title: const Text('Beginner'),
+                                leading: Radio<Levels>(
+                                  value: Levels.Beginner,
+                                  groupValue: character,
+                                  onChanged: (Levels? value) {
+                                    setState(() {
+                                      character = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              ListTile(
+                                title: const Text('Intermediate'),
+                                leading: Radio<Levels>(
+                                  value: Levels.Intermediate,
+                                  groupValue: character,
+                                  onChanged: (Levels? value) {
+                                    setState(() {
+                                      character = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              ListTile(
+                                title: const Text('Confirmed'),
+                                leading: Radio<Levels>(
+                                  value: Levels.Confirmed,
+                                  groupValue: character,
+                                  onChanged: (Levels? value) {
+                                    setState(() {
+                                      character = value;
+                                    });
+                                  },
+                                ),
+                              )
+                            ]),
+                      ],
+                    ),
+                  ))),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Container(
+              width: size.width,
+              height: 70,
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'Reset',
+                    style: TextStyle(
+                        fontSize: 18, color: Colors.blueAccent.shade700),
+                  ),
+                  buildDivider(),
+                  Text(
+                    'Apply',
+                    style: TextStyle(
+                        fontSize: 18, color: Colors.blueAccent.shade700),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Container buildDivider() {
+  return Container(
+    height: 70,
+    width: 1,
+    color: Colors.grey.shade400,
+  );
+}
---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:online_course/main.dart';
+
+void main() {
+  runApp(const SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Splash(),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  bool animation = false;
+  late AnimationController controller;
+  void initState() {
+    AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+    ).forward().then((value) => setState(() {
+          print('ads');
+          animation = true;
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void endcallback() {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (builder) => const MyApp()));
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/video-tutorials.png',
+            width: 160,
+            height: 160,
+          ),
+          AnimatedDefaultTextStyle(
+            style: animation
+                ? const TextStyle(fontSize: 40)
+                : const TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+            duration: const Duration(milliseconds: 2000),
+            onEnd: endcallback,
+            child: const Align(
+              alignment: Alignment.center,
+              child: Text('Learn Code Overcome'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
